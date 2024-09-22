87928f58bf10639d2dea83fa6bfe0e8d9a71c1dd
tenos200 <tim.enosedu@gmail.com>
2024-09-22 12:19:25 +0100

    refector into a new class, with the require function

--- main.cpp
+++ main.cpp
@@ -1,84 +1,55 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <filesystem>
 #include <iostream>
-#include <string>
 #include <unistd.h>
-#include <vector>
 
 using namespace std;
 
-int runShell() {
-    string setEnvironment = "/bin/";
-    char userInput[512];
-    int bufferLen = 512;
-    string start = "";
-    //max counter to prevent error
-    int count = 0;
-    //use a vector to decompose and store all commands
-    vector<string> allCommands;
+const int BUFFERLEN = 512;
 
-    while(count < 100) {
-        //clear the vector, for command purposes
-        allCommands.clear();
-        //showcase the first symbol  
-        printf("$");
-        //should we encounter ctrl-d check this to prevent segfault
-        if(fgets(userInput, bufferLen, stdin) == NULL) {
-                break;
-        } else {
-            start = userInput;
-            //if the user inputs exit we want to quit, otherwise we process tokens
-            if(start.compare("exit\n") == 0) {
-                break;
-            } else {
-                //split all tokens that are do not contain necessary variables
-                char *token = strtok(userInput, " \t|><;&");
 
-                //print out the valid tokens
-                while(token != NULL) {
-                    //get the next token in the list of split tokens
-                    printf("%s\n", token);
-                    if(token != NULL) {
-                        allCommands.push_back(token);
-                    }
-                    token = strtok(NULL, " \t|><;&");
-                }
+class SimpleShell {
 
-                /*
-                 * This section still needs to be fixed, we need to make sure that
-                 * args_list is constructed from the all_commands or strtok command?
-                 * Really need to read more into this..
-                 * */
-                pid_t pid = fork();
-                if(pid == 0) {
-                    //we start a new child process and then execute command
-                    string command = allCommands[0];
-                    string s = "/bin/" + command;
-                    //ISO not supported in c++11 -> need to somehow replace this
-                    char *arg_list[] = {"ls", "-a", NULL};
-                    //check if command is valid
-                    int statusCode = execvp(s.c_str(), arg_list);
-                    if(statusCode == -1) {
-                        printf("Process did not terminate correctly\n");
-                    }
-                }
-                //now this seems to always exectute before shell restarts, thats good!
-                waitpid(pid, nullptr, 0);
-            }
-        }
-        count++;
+    int processInput(char userInput[512]) {
+        //here we should write a function that gets the input, checks it
+        return -1;
     }
-    return 0;
 
-}
+    int executeCommand() {
+        //here we should fork the process and run the command in shell
+        return -1;
+    }
+
+    public: 
+        int runShell() {
+            //here we should have the while loop that is running the shell
+            int exit = 1;
+            char userInput[512];
+            while(exit == 1) {
+                printf("$ ");
+                if(fgets(userInput, BUFFERLEN, stdin) == NULL) {
+                    return -1;
+                } else if(strcmp(userInput, "exit\n") == 0) {
+                    return -1;
+                } else {
+                    //we process the input
+                    processInput(userInput);
+                }
+            }
+            return 0;
+        }
+
+};
 
 int main() {
-    runShell();
+    //initialise the shell class
+    SimpleShell shell;
+    shell.runShell();
     return 0;
-
 }
+
+
 
 
 
