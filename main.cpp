661c0b11c9f0b65e5b0cb73f30a0457e7afe3dc4
tenos200 <tim.enosedu@gmail.com>
2024-12-16 20:38:30 +0000

    added .clang-format file to home directory to properly format file with stylebased on google-style

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <unistd.h>
+
 #include <cctype>
 #include <cstdio>
 #include <cstdlib>
@@ -6,20 +8,18 @@
 #include <iostream>
 #include <string>
 #include <system_error>
-#include <unistd.h>
 #include <vector>
-#include <string> 
 using namespace std;
 
 /*
- * TODO: Stage 4 - changing directory, still requires better error handling.
+ * TODO: Stage 5 - history implementation
  * TODO: Think about a elegant way to stirp away \n from the userInput. Its
  * added in the fgets.
  *
  * */
 
-
-// Constants for the program are now defined here, unsure if this is the correct place to have them.
+// Constants for the program are now defined here, unsure if this is the correct
+// place to have them.
 const int BUFFERLEN = 512;
 const string STARTPATH = filesystem::current_path();
 const char *HOMEENVIRONMENT = getenv("HOME");
@@ -29,24 +29,24 @@ enum commandCase {
     exitCmd,
     changeDirectoryCmd,
     executableCmd,
+    historyCmd,
 };
 
 class SimpleShell {
-
-    vector<char*> processInput(char userInput[BUFFERLEN]) {
+    vector<char *> processInput(char userInput[BUFFERLEN]) {
         // Here we should write a function that gets the input, checks it
-        vector<char*> args;
+        vector<char *> args;
 
         // Replace \n with \0 from the fgets, this allows us to use execvp later
         size_t len = strlen(userInput);
         if (userInput[len - 1] == '\n') {
-          userInput[len - 1] = '\0';
+            userInput[len - 1] = '\0';
         }
 
         char *token = strtok(userInput, " \t|><;&");
         while (token != nullptr) {
-          args.push_back(token);
-          token = strtok(nullptr, " ");
+            args.push_back(token);
+            token = strtok(nullptr, " ");
         }
 
         // For execvp the last element of the array must be nullptr
@@ -54,7 +54,7 @@ class SimpleShell {
         return args;
     }
 
-    int executeCommand(vector<char*> args) {
+    int executeCommand(vector<char *> args) {
         pid_t pid = fork();
         if (pid < 0) {
             perror("fork failed");
@@ -69,15 +69,18 @@ class SimpleShell {
 
             // Debugging statement that is here for now
             cout << "Parent process: child completed" << endl;
-          }
-          return 1;
+        }
+        return 1;
     }
 
     commandCase commandChoice(string &formattedUserInput) {
-
-        if(formattedUserInput.compare("exit\n") == 0) {
+        if (formattedUserInput.compare("exit\n") == 0 ||
+            formattedUserInput.compare("exit \n") == 0) {
             return exitCmd;
-        } else if(formattedUserInput.starts_with("cd")) {
+        } else if (formattedUserInput.compare("history\n") == 0 ||
+                   formattedUserInput.compare("history \n") == 0) {
+            return historyCmd;
+        } else if (formattedUserInput.starts_with("cd")) {
             return changeDirectoryCmd;
         } else {
             return executableCmd;
@@ -93,35 +96,33 @@ class SimpleShell {
      * TODO: Add proper error handling. Look over tests with simple shell.
      *
      * */
-    void changeDirectory(string const &formattedUserInput) { 
-        error_code err{};
+    void changeDirectory(string const &formattedUserInput) {
         int stringLen = formattedUserInput.length() - 1;
         int firstNonCmdCharacter = formattedUserInput.find(" ") + 1;
         string currentPath = filesystem::current_path();
 
-        if(stringLen == 2 || stringLen == 3) {
-                filesystem::current_path(HOMEENVIRONMENT);
+        if (stringLen == 2 || stringLen == 3) {
+            filesystem::current_path(HOMEENVIRONMENT);
         } else {
             try {
-                // Concatinate string to get full path and directory 
-                string directoryCmdArgument = formattedUserInput.substr(
-                            firstNonCmdCharacter, 
-                            stringLen);
+                // Concatinate string to get full path and directory
+                string directoryCmdArgument =
+                    formattedUserInput.substr(firstNonCmdCharacter, stringLen);
 
                 // Replace final \n with \0 to pass effectively to path.
-                std::replace(directoryCmdArgument.begin(), 
-                        directoryCmdArgument.end(), '\n', '\0');
+                std::replace(directoryCmdArgument.begin(),
+                             directoryCmdArgument.end(), '\n', '\0');
 
                 filesystem::path path = directoryCmdArgument;
                 filesystem::current_path(path);
-            } catch(filesystem::filesystem_error &err) {
-                cout << err.code().message() << ": " << err.path1().c_str() 
-                    << endl;
+            } catch (filesystem::filesystem_error &err) {
+                cout << err.code().message() << ": " << err.path1().c_str()
+                     << endl;
             }
         }
     }
 
-    /* 
+    /*
      * This function removes any trailing or unnecessary whitespaces
      * */
     string removeExtraWhiteSpace(char userInput[BUFFERLEN]) {
@@ -129,67 +130,72 @@ class SimpleShell {
         string formattedString = "";
         string temp = "x";
 
-        for(char c : userInputStr) {
-            if(temp.back() != ' ' && c == ' ') {
+        for (char c : userInputStr) {
+            if (temp.back() != ' ' && c == ' ') {
                 formattedString.push_back(' ');
-            } 
+            }
             temp.push_back(c);
-            if(c != ' ') {
+            if (c != ' ') {
                 formattedString.push_back(c);
             }
         }
 
         // Given that the first character is a whitespace, remove the space
         int end = formattedString.size() - 1;
-        if(formattedString.at(0) == ' ') {
+        if (formattedString.at(0) == ' ') {
             formattedString = formattedString.substr(1, end);
         }
 
         // Lowercase all the letter
-        for(char c : formattedString) {
+        for (char c : formattedString) {
             c = tolower(c);
         }
 
         return formattedString;
     }
 
-    public:
-        int runShell() {
-            filesystem::current_path(HOMEENVIRONMENT);
-            int exit = 0;
-            char userInput[BUFFERLEN];
-            commandCase procedure;
-            string formattedUserInput;
+    void showHistory() { cout << "history" << endl; }
 
-            while (exit == 0) {
-                cout << "$ ";
-                if (fgets(userInput, BUFFERLEN, stdin) == NULL) {
-                    restorePath(STARTPATH);
-                    return -1;
-                } else {
-                    // First we format the string, removing unnecessary whitespace.
-                    formattedUserInput = removeExtraWhiteSpace(userInput);
+   public:
+    int runShell() {
+        filesystem::current_path(HOMEENVIRONMENT);
+        int exit = 0;
+        char userInput[BUFFERLEN];
+        commandCase procedure;
+        string formattedUserInput;
 
-                    // Then we get what switch statement to use.
-                    procedure = commandChoice(formattedUserInput);
-                }
+        while (exit == 0) {
+            cout << "$ ";
+            if (fgets(userInput, BUFFERLEN, stdin) == NULL) {
+                restorePath(STARTPATH);
+                return -1;
+            } else {
+                // First we format the string, removing unnecessary whitespace.
+                formattedUserInput = removeExtraWhiteSpace(userInput);
 
-                switch (procedure) {
-                    case exitCmd:
-                        exit = restorePath(STARTPATH);
-                        break;
-                    case changeDirectoryCmd:
-                        changeDirectory(formattedUserInput);
-                        break;
-                    case executableCmd:
-                        vector<char*> args = processInput(userInput);
-                        executeCommand(args);
-                        break;
-                }
+                // Then we get what switch statement to use.
+                procedure = commandChoice(formattedUserInput);
             }
-            filesystem::current_path(STARTPATH);
-            return -1;
+
+            switch (procedure) {
+                case exitCmd:
+                    exit = restorePath(STARTPATH);
+                    break;
+                case changeDirectoryCmd:
+                    changeDirectory(formattedUserInput);
+                    break;
+                case historyCmd:
+                    showHistory();
+                    break;
+                case executableCmd:
+                    vector<char *> args = processInput(userInput);
+                    executeCommand(args);
+                    break;
+            }
         }
+        filesystem::current_path(STARTPATH);
+        return -1;
+    }
 };
 
 int main() {
